bc6e9958839bb8c6f3b3b99f6e57c2e483502cff
Thuang <thomashuang.th@gmail.com>
2016-06-15 15:24:37 +0800

    Adding contacts
    * Contact related to users
    * Contacts are editable

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,8 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\User;
+use AppBundle\Entity\Contact;
 
 class DefaultController extends Controller
 {
@@ -13,9 +15,89 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $contacts = $this->showContacts();
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+            'contacts' => $contacts,
         ));
+    }
+
+    /**
+     * @Route("/createtask", name="create_task")
+     */
+    public function createContactAction()
+    {
+        $request = Request::createFromGlobals();
+        $user = $this->getUser();
+
+        $contact = new Contact();
+        $contact->setEmail($request->request->get('email'));
+        $contact->setAddress($request->request->get('address'));
+        $contact->setPhone($request->request->get('phoneNumber'));
+        $contact->setWebsite($request->request->get('website'));
+
+        $contact->setFosUser($user);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($contact);
+        $em->flush();
+
+        $contacts = $this->showContacts();
+
+        return $this->redirectToRoute('homepage');
+    }
+
+    public function showContacts() {
+        $user = $this->getUser();
+
+        $contacts = $user->getContacts();
+
+        return $contacts;
+    }
+
+    /**
+     * @Route("/task/{id}", name="edit_page")
+     */
+    public function editAction($id)
+    {
+        $contact = $this->getDoctrine()
+            ->getRepository('AppBundle:Contact')
+            ->find($id);
+
+        if (!$contact) {
+            throw $this->createNotFoundException(
+                'No contact found for id '.$id
+            );
+        }
+
+        return $this->render('default/edit.html.twig', array('contact' => $contact));
+    }
+
+    /**
+     * @Route("/edittask/{id}", name="edit_task")
+     */
+    public function editContactAction($id)
+    {
+        $request = Request::createFromGlobals();
+        $em = $this->getDoctrine()->getManager();
+        $contact = $em
+            ->getRepository('AppBundle:Contact')
+            ->find($id);
+
+        if (!$contact) {
+            throw $this->createNotFoundException(
+                'No contact found for id '.$id
+            );
+        }
+
+        $contact->setEmail($request->request->get('email'));
+        $contact->setAddress($request->request->get('address'));
+        $contact->setPhone($request->request->get('phoneNumber'));
+        $contact->setWebsite($request->request->get('website'));
+        $em->flush();
+
+        return $this->redirectToRoute('homepage');
     }
 }
---
+++ src/AppBundle/Entity/Contact.php
@@ -0,0 +1,188 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Contact
+ *
+ * @ORM\Table(name="contact")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ContactRepository")
+ */
+class Contact
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255, unique=true)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="address", type="string", length=255, nullable=true)
+     */
+    private $address;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="phone", type="string", length=255)
+     */
+    private $phone;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="website", type="string", length=255, nullable=true)
+     */
+    private $website;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="User", inversedBy="contacts")
+     * @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     */
+    private $fos_user;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return Contact
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set address
+     *
+     * @param string $address
+     *
+     * @return Contact
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    
+        return $this;
+    }
+
+    /**
+     * Get address
+     *
+     * @return string
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * Set phone
+     *
+     * @param string $phone
+     *
+     * @return Contact
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+    
+        return $this;
+    }
+
+    /**
+     * Get phone
+     *
+     * @return string
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * Set website
+     *
+     * @param string $website
+     *
+     * @return Contact
+     */
+    public function setWebsite($website)
+    {
+        $this->website = $website;
+    
+        return $this;
+    }
+
+    /**
+     * Get website
+     *
+     * @return string
+     */
+    public function getWebsite()
+    {
+        return $this->website;
+    }
+
+    /**
+     * Set fosUser
+     *
+     * @param \AppBundle\Entity\User $fosUser
+     *
+     * @return Contact
+     */
+    public function setFosUser(\AppBundle\Entity\User $fosUser = null)
+    {
+        $this->fos_user = $fosUser;
+    
+        return $this;
+    }
+
+    /**
+     * Get fosUser
+     *
+     * @return \AppBundle\Entity\User
+     */
+    public function getFosUser()
+    {
+        return $this->fos_user;
+    }
+}
